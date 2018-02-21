a84aad98a17906ab03e8f24fb873f6d89fd82959
Iván Meléndez <argordmel@yahoo.es>
2018-02-21 11:47:38 -0500

    [Update] Webpack Config

--- webpack.base.js
+++ webpack.base.js
@@ -9,7 +9,7 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: [
-            'es2015',
+            'env',
             'stage-1'
           ]
         }
