79aa490dc67e7b46e2b2292e6776462d782572c1
Iván Meléndez <argordmel@yahoo.es>
2018-02-21 23:03:44 -0500

    [Updated] Examples

--- app/controllers/WelcomeController.js
+++ app/controllers/WelcomeController.js
@@ -1,4 +1,4 @@
-/* global Example */
+/* global Example, VSError */
 
 /**
  * WelcomeController.js
@@ -12,29 +12,68 @@ module.exports = {
 
   },
 
+  'get :id': (req, res) => {
+
+    const id = req.params.id || null;
+    Example.findOne({ _id: id }).then( (example) => {
+      if (!example) {
+        res.vsr(Promise.reject( (new VSError('Object Not Found', 404)) ));
+        return null;
+      }
+      res.vsr(Promise.resolve(example));
+      return null;
+    }).catch( (err) => {
+      res.vsr(Promise.reject( (new VSError(err, 500)) ));
+    });
+
+  },
+
   post: (req, res) => {
 
     const data = req.body || {};
     const obj = new Example(data);
-    res.json(obj.save());
+    res.vsr(obj.save(), 201);
 
   },
 
   'put :id': (req, res) => {
 
-    res.json({ hello: 'Put Method!' });
+    const data = req.body || {};
+    const id = req.params.id || null;
+    Example.findOne({ _id: id }).then( (example) => {
+      if (!example) {
+        res.vsr(Promise.reject( (new VSError('Object Not Found', 404)) ));
+        return null;
+      }
+      const merge = Object.assign(example, data);
+      const result = Example.findOneAndUpdate({ _id: id }, merge, { new: true });
+      res.vsr(result, 202);
+      return null;
+    }).catch( (err) => {
+      res.vsr(Promise.reject( (new VSError(err, 500)) ));
+    });
 
   },
 
   'delete :id': (req, res) => {
 
-    res.json({ hello: 'Delete Method!' });
+    const id = req.params.id || null;
+    Example.findOne({ _id: id }).then( (example) => {
+      if (!example) {
+        res.vsr(Promise.reject( (new VSError('Object Not Found', 404)) ));
+        return null;
+      }
+      res.vsr(Example.remove({ _id: id }), 204);
+      return null;
+    }).catch( (err) => {
+      res.vsr(Promise.reject( (new VSError(err, 500)) ));
+    });
 
   },
 
-  'put custom/:id': (req, res) => {
+  'post custom/:id': (req, res) => {
 
-    res.json({ hello: 'Custom Put Method!' });
+    res.vsr(Promise.resolve({ hello: 'Custom Post Method!' }));
 
   }
 
