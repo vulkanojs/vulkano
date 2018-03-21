86cb05facdb20a5b6dbe37a4ca58cea72cacbea6
Iván Meléndez <argordmel@yahoo.es>
2018-03-21 16:34:41 -0500

    [fixed] Removed token via query parameter

--- app/services/Jwt.js
+++ app/services/Jwt.js
@@ -2,26 +2,25 @@
 
 const jwt = require('express-jwt');
 const jwtSimple = require('jwt-simple');
-const _ = require('underscore');
 
 module.exports = {
 
   init: (opts) => {
 
     const key = app.config.jwt.key || '';
-    const header = app.config.jwt.header || 'x-token-auth';
-    const param = app.config.jwt.queryParameter || 'token';
+    const header = app.config.jwt.header || 'x-token-auth' || '';
+    const param = app.config.jwt.queryParameter || '';
     const config = {
       secret: key,
       getToken: function fromHeaderOrQuerystring(req) {
         return req.headers[header] ||
                 req.headers[header.toUpperCase()] ||
-                (req.query && req.query[param.toLowerCase()]) ||
+                (param && req.query && req.query[param.toLowerCase()]) ||
                 null;
       }
     };
 
-    return jwt(_.extend(config, opts));
+    return jwt(Object.assign({}, config, opts));
 
   },
 
