c521ecc9d7e384c2f081799686185b7dfe1a84cb
Iván Meléndez <argordmel@yahoo.es>
2018-03-21 15:39:00 -0500

    [chore] Clean file

--- app/config/connections.js
+++ app/config/connections.js
@@ -6,10 +6,6 @@ module.exports = {
 
   ivanmel: 'mongodb://localhost/vulcano',
 
-  josedaniel: 'mongodb://localhost/vulcano',
-
-  romel: 'mongodb://localhost/vulcano',
-
   development: 'mongodb://server/database',
 
   production: 'mongodb://server/database'
