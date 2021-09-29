1bcc64448bdcf02738add3108568c2137d009787
Iván Meléndez <argordmel@yahoo.es>
2021-09-29 17:42:11 -0500

    [updated] Deploy with PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+
+  apps: [
+    {
+      name: 'vulkano',
+      script: 'app.js',
+      max_memory_restart: '800M',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      env: {
+        PORT: 5000,
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'vulkano-staging',
+      script: 'app.js',
+      max_memory_restart: '800M',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      env: {
+        PORT: 5100,
+        NODE_ENV: 'development'
+      }
+    }
+  ],
+
+  deploy: {
+
+    production: {
+      user: 'SSH_USERNAME',
+      port: 'SSH_PORT',
+      host: ['SSH_HOSTMACHINE'],
+      ref: 'origin/production',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-setup': 'echo "commands or local script path to be run on the host before the setup process starts"',
+      'post-deploy': 'echo yes | npm install && pm2 startOrRestart ecosystem.config.js --only vulkano && pm2 save',
+      env: {
+        NODE_ENV: 'production'
+      }
+    },
+
+    staging: {
+      user: 'SSH_USERNAME',
+      port: 'SSH_PORT',
+      host: ['SSH_HOSTMACHINE'],
+      ref: 'origin/production',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-setup': 'echo "commands or local script path to be run on the host before the setup process starts"',
+      'post-deploy': 'echo yes | npm install && pm2 startOrRestart ecosystem.config.js --only vulkano-staging && pm2 save',
+      env: {
+        NODE_ENV: 'development'
+      }
+    }
+
+  }
+
+};
