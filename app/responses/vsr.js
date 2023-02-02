3496275a8ae6372f757d537d2e15992a2a154dde
Iván Meléndez <argordmel@yahoo.es>
2023-02-02 12:19:48 -0500

    [updated] Debug promises

--- app/responses/vsr.js
+++ app/responses/vsr.js
@@ -4,23 +4,24 @@
 
 const Promise = require('bluebird');
 
-module.exports = function VSRPromise(prom, okcode) {
+module.exports = function VSRPromise(promiseToRun, httpStatusCode) {
 
   const {
     req
   } = this;
 
   const {
-    res
+    res,
+    path
   } = req;
 
-  let code = okcode || 200;
+  let code = httpStatusCode || 200;
   let output = {
     success: true,
     statusCode: code
   };
 
-  if (!prom || !prom.then) {
+  if (!promiseToRun || !promiseToRun.then) {
 
     // Log error to console
     console.log('The response is not a promise');
@@ -34,47 +35,58 @@ module.exports = function VSRPromise(prom, okcode) {
   }
 
   // Executing promise
-  prom.then( (r) => {
+  promiseToRun
+    .then( (r) => {
 
-    if ( (r.statusCode && r.statusCode >= 400) || output.statusCode >= 400) {
+      if ( (r.statusCode && r.statusCode >= 400) || output.statusCode >= 400) {
 
-      if (r.statusCode && r.statusCode !== 402) {
-        return Promise.reject(r);
+        if (r.statusCode && r.statusCode !== 402) {
+          return Promise.reject(r);
+        }
+
+        if ( output.statusCode >= 400 && output.statusCode !== 402 ) {
+          return Promise.reject(r);
+        }
+
+        output.statusCode = r.statusCode;
+        code = r.statusCode;
+
       }
 
-      if ( output.statusCode >= 400 && output.statusCode !== 402 ) {
-        return Promise.reject(r);
+      if (r && r.serialized) { // if the object has serialized? see services/Serializer
+        output = { ...output, ...r };
+        delete output.serialized;
+      } else {
+        output.data = r;
       }
 
-      output.statusCode = r.statusCode;
-      code = r.statusCode;
+      return true;
 
-    }
+    })
+    .catch( (e) => {
 
-    if (r && r.serialized) { // if the object has serialized? see services/Serializer
-      output = Object.assign({}, output, r);
-      delete output.serialized;
-    } else {
-      output.data = r;
-    }
+      if (path) {
+        console.log('ERROR PATH', path);
+      }
 
-    return true;
+      if (!app.PRODUCTION) {
+        console.log(e);
+      }
 
-  }).catch( (e) => {
+      const message = e.message !== undefined && typeof e.message === 'object' ? e.message : e;
+      code = message.statusCode || e.statusCode || 400;
 
-    const message = e.message !== undefined && typeof e.message === 'object' ? e.message : e;
-    code = message.statusCode || e.statusCode || 400;
+      // Output
+      output.success = false;
+      output.statusCode = code;
+      output.error = {
+        errorCode: message.code || '001',
+        errorName: message.name || 'BadRequest',
+        detail: message.message || message.error || message.invalidAttributes || message.toString(),
+        output: message
+      };
 
-    // Output
-    output.success = false;
-    output.statusCode = code;
-    output.error = {
-      errorCode: message.code || '001',
-      errorName: message.name || 'BadRequest',
-      detail: message.message || message.error || message.invalidAttributes || message.toString(),
-      output: message
-    };
-
-  }).finally( () => res.status(code).jsonp(output));
+    })
+    .finally( () => res.status(code).jsonp(output) );
 
 };
