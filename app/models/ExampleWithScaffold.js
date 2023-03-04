7379f422dca8d08d3dcc20f29f3224ddf959d1d0
Iván Meléndez <argordmel@yahoo.es>
2023-03-03 19:29:23 -0500

    [updated] Docs

--- app/models/ExampleWithScaffold.js
+++ app/models/ExampleWithScaffold.js
@@ -16,15 +16,14 @@ module.exports = {
     // the fields:
     // active, createdAt, updatedAt
     // was created automatically
-  }
+  },
 
   // Scaffold (ModelName) - Allowed Methods:
   // ExampleWidthScaffold.getAllExampleWidthScaffold(props)
-  // ExampleWidthScaffold.getAll(props) (alias)
+  // ExampleWidthScaffold.getAll(props) (alias to getAllModelName)
   // ExampleWidthScaffold.create(payload)
-  // ExampleWidthScaffold.getExampleWidthScaffold(id)
   // ExampleWidthScaffold.getByField(email, 'email') (alias)
-  // ExampleWidthScaffold.getById(id) (alias)
+  // ExampleWidthScaffold.getExampleWidthScaffold(id)
   // ExampleWidthScaffold.update(id, payload)
   // ExampleWidthScaffold.delete(id)
 
