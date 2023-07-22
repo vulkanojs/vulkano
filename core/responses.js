83e1eda4a7c3f4dc84cdbe27e92d3ae71bf90b98
Iván Meléndez <argordmel@yahoo.es>
2023-07-21 20:36:53 -0500

    [updated] Moved Responses

--- core/responses.js
+++ core/responses.js
@@ -1,17 +1,28 @@
 const path = require('path');
 
 // Include all user's responses
-const AllResponses = require('include-all')({
+const VulkanoResponses = require('include-all')({
+  dirname: path.normalize(path.join(__dirname, 'responses')),
+  optional: true
+});
+
+const CustomResponses = require('include-all')({
   dirname: path.normalize(path.join(__dirname, '../app/responses')),
   optional: true
 });
 
 module.exports = function loadResponsesApplication(req, res, next) {
 
-  // add user reponses
-  Object.keys(AllResponses).forEach((response) => {
+  // add custom user reponses
+  Object.keys(VulkanoResponses || {}).forEach((response) => {
     const reponseName = response.split('.')[0];
-    res[reponseName] = AllResponses[response];
+    res[reponseName] = VulkanoResponses[response];
+  });
+
+  // add custom user reponses
+  Object.keys(CustomResponses || {}).forEach((response) => {
+    const reponseName = response.split('.')[0];
+    res[reponseName] = CustomResponses[response];
   });
 
   next();
