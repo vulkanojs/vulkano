c6b718a3c999c356f6f4877f7dd675031514b919
Iván Meléndez <argordmel@yahoo.es>
2024-04-11 10:26:40 -0500

    [updated] Server entry point

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 /**
- * vulkano.js
+ * app.js
  *
- * To start the server, run: `node vulkano.js`.
+ * To start the server, run: `node app.js`.
  *
  * For example:
  *   => `npm run start`
- *   => `node vulkano.js`
+ *   => `node app.js`
  */
-
-const path = require('path');
-
-global.START_TIME = new Date();
-
-global.ABS_PATH = path.resolve(__dirname, './');
-global.APP_PATH = path.join(__dirname, './app');
-global.PUBLIC_PATH = path.join(__dirname, './public');
 
 const vulkano = require('@vulkano/core');
 
