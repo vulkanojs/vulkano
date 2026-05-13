03801f4dc4ffb7dc51ea389699463e356c1426a7
Iván Meléndez <argordmel@yahoo.es>
2026-05-13 10:12:34 -0500

    [added ] Comments to Helmet config for clarity

--- app/config/express/helmet.js
+++ app/config/express/helmet.js
@@ -1,11 +1,27 @@
+/**
+ *
+ * Helmet Config
+ * Documentation: https://helmetjs.github.io/#reference
+ *
+ */
+
 module.exports = {
 
+  //
+  // @type Object
+  // 
   referrerPolicy: {
     policy: 'strict-origin-when-cross-origin'
   },
 
+  //
+  // @type Boolean
+  // 
   contentSecurityPolicy: false,
 
+  //
+  // @type Boolean
+  // 
   crossOriginEmbedderPolicy: false
 
 };
