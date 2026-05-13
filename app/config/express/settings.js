c052830df141134f48976f2c9d1a5556b3ec1975
Iván Meléndez <argordmel@yahoo.es>
2026-05-13 10:06:32 -0500

    [updated] Add trustProxy setting to Express configuration

--- app/config/express/settings.js
+++ app/config/express/settings.js
@@ -1,3 +1,8 @@
+/**
+ * Express Config
+ *
+ * Custom Express Config
+ */
 module.exports = {
 
   // Show powered by in http headers
@@ -7,6 +12,12 @@ module.exports = {
   timeout: 120000,
 
   // Folder to upload files
-  uploadPath: 'public/files'
+  uploadPath: 'public/files',
+
+  // Number of proxy hops to trust for X-Forwarded-* headers.
+  // Use 1 when behind a single load balancer, true to trust all (less secure).
+  // See https://expressjs.com/en/guide/behind-proxies.html
+  // @type Number | Boolean
+  trustProxy: 1
 
 };
