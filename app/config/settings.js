4bcd7a67ca1803f30b1beff314155882923e873c
Iván Meléndez <argordmel@yahoo.es>
2026-05-13 10:03:17 -0500

    [updated] Cleaning code

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,26 +1,12 @@
 /**
- * Custom Server Config
+ * Server Config
  *
- * host
- * port
- * config: x-powered-by
- * ...
- * ...
- *
- *
- * This config is merged with Express Config
  */
 
 module.exports = {
 
-  // Custom Server Config
-  host: process.env.HOST || '',
-
   // Port
   port: process.env.PORT || 8000,
-
-  // Salt for password
-  salt: process.env.SALT_KEY || '',
 
   // Connection to Database
   database: {
