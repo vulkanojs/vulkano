7406eb56f499bb91f7c309097142d936b938774f
Iván Meléndez <argordmel@yahoo.es>
2026-05-13 10:09:28 -0500

    [added] Enhance comments in Express JSON config
    Added comments to clarify the purpose of the JSON config.

--- app/config/express/json.js
+++ app/config/express/json.js
@@ -1,7 +1,19 @@
+/**
+ *
+ * Express JSON Config
+ * https://expressjs.com/en/api.html#express.json
+ *
+ */
 module.exports = {
+  
+  // 
+  // This is used to determine what media type the middleware will parse.
+  // @type Array
+  // 
   type: [
     'application/json',
     'application/csp-report',
     'application/reports+json',
   ]
+  
 };
