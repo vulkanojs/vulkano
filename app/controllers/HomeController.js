c6192504e89599d6edd91fd8fc0757ed40a62d5c
Iván Meléndez <argordmel@yahoo.es>
2026-05-26 11:01:16 -0500

    [updated] Vite support from core

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,18 +1,8 @@
-/**
- * HomeController.js
- */
-
 module.exports = {
 
   get(req, res) {
 
     res.render('home/index.html');
-
-  },
-
-  cms(req, res) {
-
-    res.render('home/cms.html');
 
   },
 
