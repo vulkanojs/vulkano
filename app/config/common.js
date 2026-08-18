9012ce8739be32ba3304fc025d7d07260940bf75
Iván Meléndez <argordmel@yahoo.es>
2026-08-18 13:23:33 -0500

    [updated] MD files

---
+++ app/config/common.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SEO_DEFAULT_TITLE: 'Vulkano | Framework for building full-stack apps',
+  SEO_DEFAULT_DESCRIPTION: 'Full-stack app built with the Vulkano Framework.',
+  SEO_DEFAULT_IMAGE: '/favicon.png'
+};
---
+++ app/config/middlewares/seo.js
@@ -0,0 +1,12 @@
+module.exports = (req, res, next) => {
+
+  res.locals.seo = {
+    title: app.config.common.SEO_DEFAULT_TITLE,
+    description: app.config.common.SEO_DEFAULT_DESCRIPTION,
+    image: app.config.common.SEO_DEFAULT_IMAGE,
+    url: `${req.protocol}://${req.get('host')}${req.originalUrl}`
+  };
+
+  next();
+
+};
